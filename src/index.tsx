6da210a2c8d7e5ba6884070ee528f333a919e5cd
David Prabu <davidprabu@dorustree.in>
2022-09-15 10:26:07 +0530

    Routes changed

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import store from "./store/ReduxStore";
 
 const root = ReactDOM.createRoot(
@@ -12,11 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <>
     <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="*" element={<App />} />
-        </Routes>
-      </BrowserRouter>
+     <App />
     </Provider>
   </>
 );
